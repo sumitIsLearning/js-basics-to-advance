21836f783fe2de68f474445ce3864506837db53b
sumitIsLearning <try.sumitchakraborty@gmail.com>
2024-09-03 15:13:21 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+/*
+ string , Number, boolean , null , undefined  , Symbol, BigInt
+ These are all primative data types
+ objects is a non-primative data type
+*/
+
+/*
+console.log(typeof 2); // number
+console.log(typeof "s");// string
+console.log(typeof true);// boolean
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+console.log(typeof Symbol);// function
+console.log(typeof BigInt);// function
+*/
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "zenx@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lumding";
+let accountState;
+
+// accountId = 2;
+accountEmail = "ajfk@gmail.com";
+accountPassword = "2414141";
+accountCity = "Guwahati";
+
+// console.log(accountId);
+console.table({accountId, accountEmail, accountPassword , accountCity, accountState});
+
+
