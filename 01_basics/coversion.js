75434b16aea6ba06c8e3f1694952885db6b659a1
sumitIsLearning <try.sumitchakraborty@gmail.com>
2024-09-08 14:53:23 +0000

    learned about conversion and comparision

---
+++ 01_basics/coversion.js
@@ -0,0 +1,20 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+
+// let valueInNumber = parseInt(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = "sumit";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "s" => true
+// 1 => true
+// 0 => false
+
+
