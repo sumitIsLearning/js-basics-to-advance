a99a4d7b715a8836adbc30ae9a4761d221512531
sumitIsLearning <try.sumitchakraborty@gmail.com>
2024-10-01 15:19:51 +0000

    got a in depth knowledge about how scope works in js and how functions can be declared in many ways

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -3,7 +3,41 @@ let a = 20;
 
 if(true) {
     let a = 2;
-    console.log("Blocked scope: ", a);
+    // console.log("Blocked scope: ", a);
 }
 
-console.log("Global scope: ", a);
+// console.log("Global scope: ", a);
+
+
+function one() {
+    const username = "Sumit";
+    const two = () => {
+        const website = "Youtube.com";
+        console.log(username);
+    }
+    // console.log(website); // It will give us reference error: website is not defined;
+
+    two();
+}
+
+// one();
+
+if(true) {
+    const username = "Sumit";
+    if(true) {
+        const website = "Youtube";
+        console.log(`${username}'s ${website}`);
+    }
+    // console.log(website); // first reference error
+}
+
+// console.log(username); // second reference error
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+// sometime this is known as expressions
+const addOne = (num) => {
+    return num + 1;
+} // Note: if you try to access this type of functions before declaration , it will througn referenceerror: cannot access 'function name' before initialization
+
+console.log(addOne(4));
